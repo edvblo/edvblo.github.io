f190665594a9f88ace1a8c73e14d7d4ab7aebca5
@oi22ebg <oi22ebg@cs.umu.se>
2026-04-05 13:08:44 +0200

    task2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
     alert("Hello, JavaScript!");
 }
+
+function showInfo(id) {
+    const info = document.getElementById(id);
+    info.style.display = info.style.display === "none" ? "block" : "none";
+  }
