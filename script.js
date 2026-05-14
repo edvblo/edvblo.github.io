5e6e1612f78be7c9d1011ab478135dffbc847edd
@oi22ebg <oi22ebg@cs.umu.se>
2026-05-14 18:55:11 +0200

    made task 2, movie-scroll

--- script.js
+++ script.js
@@ -2,10 +2,35 @@ function showMessage() {
     alert("Hello, JavaScript!");
 }
 
-function openPopup(id) {
-    document.getElementById(id).style.display = 'flex';
+const movieRow = document.getElementById("movieRow");
+const movies = document.querySelectorAll(".movie-card");
+
+const movieWidth = 300;
+
+let position = 0;
+function moveLeft() {
+    const step = 300;
+    const maxScroll =
+        movieRow.parentElement.offsetWidth - movieRow.scrollWidth;
+
+    position += step;
+
+    if (position > 0) {
+        position = maxScroll;
+    }
+
+    movieRow.style.transform = `translateX(${position}px)`;
 }
-  
-function closePopup(id) {
-    document.getElementById(id).style.display = 'none';
+function moveRight() {
+    const step = 300;
+    const maxScroll =
+        movieRow.parentElement.offsetWidth - movieRow.scrollWidth;
+
+    position -= step;
+
+    if (position < maxScroll) {
+        position = 0;
+    }
+
+    movieRow.style.transform = `translateX(${position}px)`;
 }
